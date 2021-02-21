9392b05208a67bf500ce88dc794ae4fb45d95861
lukasmas <mastalerzl12@gmail.com>
2021-02-21 16:33:29 +0100

    Setup the project

---
+++ README.md
@@ -0,0 +1,16 @@
+# SOIC - Simulator of IoT Communication
+
+## 0. Setup
+```
+$ python3 -m venv env
+$ source env/bin/activate 
+$ pip install -r requirements.txt
+```
+
+## 1. Usage
+```
+TBA...
+```
+
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+
+def print_soic():
+    print('Welcome in SOIC, work are in progress...')  # Pres
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_soic()
+
+
