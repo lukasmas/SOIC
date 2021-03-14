66881d70925528599a7a9a07e8d00bef4c840b2d
lukasmas <mastalerzl12@gmail.com>
2021-03-14 12:14:22 +0100

    adding option to read configuration from json

--- Node.py
+++ Node.py
@@ -1,4 +1,6 @@
 import signal
+from random import randrange
+
 import zmq
 import time
 import argparse
@@ -23,23 +25,23 @@ class Node:
     def __init__(self, sub_list, pub_list):
         self.type = NodeType.Null
         signal.signal(signal.SIGINT, signal.SIG_DFL)
-        context = zmq.Context()
+        sub_context = zmq.Context()
+        pub_context = zmq.Context()
         self.pub_list = pub_list
-        if sub_list and pub_list:
-            self.type = NodeType.Both
-            print('Not supported yet!')
-            return
-        if sub_list:
-            self.type = NodeType.Subscriber
-            self.socket = context.socket(zmq.SUB)
-            for sub_point in sub_list:
-                self.socket.connect('tcp://localhost:' + str(sub_point))
-            self.socket.setsockopt(zmq.SUBSCRIBE, b'okon')
         if pub_list:
             self.type = NodeType.Publisher
-            self.socket = context.socket(zmq.PUB)
+            self.pub_socket = pub_context.socket(zmq.PUB)
             for pub_point in pub_list:
-                self.socket.bind('tcp://*:' + str(pub_point))
+                self.pub_socket.bind('tcp://*:' + str(pub_point))
+        if sub_list:
+            if self.type == NodeType.Publisher:
+                self.type = NodeType.Both
+            else:
+                self.type = NodeType.Subscriber
+            self.sub_socket = sub_context.socket(zmq.SUB)
+            for sub_point in sub_list:
+                self.sub_socket.connect('tcp://localhost:' + str(sub_point))
+            self.sub_socket.setsockopt(zmq.SUBSCRIBE, b'okon')
         self.list = []
         print("Node type " + self.type.name + " created!")
 
@@ -48,17 +50,24 @@ class Node:
             self.listen_msg()
         if self.type == NodeType.Publisher:
             self.send_msg()
+        if self.type == NodeType.Both:
+            n = randrange(2)
+            print(n)
+            if n == 0:
+                self.send_msg()
+            else:
+                self.listen_msg()
 
     def listen_msg(self):
         while True:
-            message = self.socket.recv_multipart()
+            message = self.sub_socket.recv_multipart()
             print(f'Received: {message}')
             self.list.append(message)
 
     def send_msg(self):
         msg_counter = 0
         while True:
-            self.socket.send(b'okon msg ' + str.encode(str(msg_counter)))
+            self.pub_socket.send(b'okon msg ' + str.encode(str(msg_counter)))
             msg_counter += 1
             time.sleep(3)
 
--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-import subprocess
+import multiprocessing as mp
+import json
+
 
 def print_soic():
     print('Welcome in SOIC, work are in progress...')  # Pres
 
 
+def read_config():
+    with open('connections.json') as json_file:
+        data = json.load(json_file)
+        return data['connections']
+
+
+def parse_configuration(config, base_port=5000):
+    print("Parsing nodes connections:")
+    configuration_list = []
+    port = base_port
+    for c in config:
+        index = 0
+        sub_list = []
+        for sub in c:
+            if sub == 1:
+                sub_list.append(base_port + index)
+            index = index + 1
+        print(f'pub: {port}, sub: {sub_list}')
+        configuration_list.append((port, sub_list))
+        port = port + 1
+    return configuration_list
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_soic()
-    subprocess.run("python Node.py --sub 5001")
-    subprocess.run("python Node.py --pub 5001")
+    raw_configuration = read_config()
+    configuration = parse_configuration(raw_configuration)
+    # subprocess.run("python Node.py --sub 5001")
+    # subprocess.run("python Node.py --pub 5001")
 
-
+    # multiprocess
